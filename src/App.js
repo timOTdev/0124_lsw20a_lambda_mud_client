98d6a5a77a4ce3140904c8825a41997949220d8d
Timothy Hoang <timh1203@gmail.com>
2018-12-13 15:43:04 -0600

    Add attempt for rotating main images

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ import Signup from './components/Signup'
 import Login from './components/Login'
 import Main from './components/Main'
 import axios from 'axios'
-import background from './assets/images/background.jpg'
+// import background from './assets/images/background.jpg'
+// import evilVeil from './assets/images/evilVeil.jpg'
+import heroes from './assets/images/heroes.jpg'
+import necro from './assets/images/necro.jpg'
+import onslaught from './assets/images/onslaught.jpg'
+import plagueDoctor from './assets/images/plagueDoctor.jpg'
+// import priest from './assets/images/priest.jpg'
+import shaman from './assets/images/shaman.jpg'
 require('dotenv').config()
 
 // Styled-Components
 const Div1 = styled.div`
-  background-image: url(${background});
-  background-size: cover;
-  background-repeat: no-repeat;
+  /* background-image: url(); */
+  /* background-size: cover; */
+  /* background-repeat: no-repeat; */
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -22,6 +29,69 @@ const Div1 = styled.div`
   align-items: center;
   color: #2C7FCC;
   font-family: "Julee";
+
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 0;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`
+const Div2 = styled.div`
+    * {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-size: cover;
+      background-position: 50% 50%;
+      background-repeat: no-repeat;
+      opacity: 0;
+      z-index: 0;
+      animation: imageAnimation 50s linear infinite; 
+    }
+    li:nth-child(1){
+      background-image: url(${shaman});
+    }
+    li:nth-child(2){
+      background-image: url(${heroes});
+      animation-delay: 10s; 
+    }
+    li:nth-child(3){
+      background-image: url(${necro});
+      /* animation-delay: 20s;  */
+    }
+    li:nth-child(4){
+      background-image: url(${onslaught});
+      /* animation-delay: 30s;  */
+    }
+    li:nth-child(5){
+      background-image: url(${plagueDoctor});
+      /* animation-delay: 40s;  */
+    }
+    @keyframes imageAnimation { 
+      0% { 
+        opacity: 0; 
+        animation-timing-function: ease-in;
+      }
+      10% {
+        opacity: 1;
+        animation-timing-function: ease-out;
+      }
+      20% {
+        opacity: 1;
+      }
+      30% {
+        opacity: 0;
+      }
+    }
+`
+const Div3 = styled.div`
+    z-index: 1;
 `
 
 export default class App extends Component {
@@ -88,37 +158,55 @@ export default class App extends Component {
   render() {
     return (
       <Div1>
-        {
-          !this.state.isLoggedIn && !this.state.isCreatingUser ? (
-            <>
-              <h1>Lambda MUD</h1>
-              <Login
-                toggleCreateUserForm={this.toggleCreateUserForm}
-                login={this.login}
+        <Div2>
+          <ul>
+            {/* <li>{evilVeil}</li>
+            <li>{heroes}</li>
+            <li>{necro}</li>
+            <li>{onslaught}</li>
+            <li>{plagueDoctor}</li>
+            <li>{priest}</li>
+            <li>{shaman}</li> */}
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </Div2>
+        <Div3>
+          {
+            !this.state.isLoggedIn && !this.state.isCreatingUser ? (
+              <>
+                <h1>Lambda MUD</h1>
+                <Login
+                  toggleCreateUserForm={this.toggleCreateUserForm}
+                  login={this.login}
+                />
+              </>
+            ) : null
+          }
+          {
+            !this.state.isLoggedIn && this.state.isCreatingUser ? (
+              <>
+                <h1>Signup</h1>
+                <Signup
+                  toggleCreateUserForm={this.toggleCreateUserForm}
+                  signup={this.signup}
+                />
+              </>
+            ) : null
+          }
+          {
+            this.state.isLoggedIn ? (
+              <Main
+                isLoggedIn={this.state.isLoggedIn}
+                initialize={this.initialize}
+                logout={this.logout}
               />
-            </>
-          ) : null
-        }
-        {
-          !this.state.isLoggedIn && this.state.isCreatingUser ? (
-            <>
-              <h1>Signup</h1>
-              <Signup
-                toggleCreateUserForm={this.toggleCreateUserForm}
-                signup={this.signup}
-              />
-            </>
-          ) : null
-        }
-        {
-          this.state.isLoggedIn ? (
-            <Main
-              isLoggedIn={this.state.isLoggedIn}
-              initialize={this.initialize}
-              logout={this.logout}
-            />
-          ) : null
-        }
+            ) : null
+          }
+        </Div3>
       </Div1>
     );
   }
